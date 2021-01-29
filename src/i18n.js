ce1c262621f91b9a1c0b23ba909019b84ac9a961
Marcial Cambronero <mcambronero@backcountry.com>
2021-01-29 00:14:11 -0600

    Revamp meta tags and initial setup for PWA

--- src/i18n.js
+++ src/i18n.js
@@ -1,16 +1,15 @@
-import { addMessages, init, getLocaleFromNavigator } from 'svelte-i18n';
-import enLocale from './locales/en.json';
-import esLocale from './locales/es.json';
+import { addMessages, init, getLocaleFromNavigator } from "svelte-i18n";
+import enLocale from "./locales/en.json";
+import esLocale from "./locales/es.json";
 
 function initializeI18n() {
-  addMessages('en', enLocale);
-  addMessages('es', esLocale);
-  
+  addMessages("en", enLocale);
+  addMessages("es", esLocale);
+
   init({
-    fallbackLocale: 'en',
+    fallbackLocale: "en",
     initialLocale: getLocaleFromNavigator(),
   });
 }
-
 
 export default initializeI18n;
